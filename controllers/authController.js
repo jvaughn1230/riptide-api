25462136c1c587fe479dfa74937ff35af85ec094
Alex <jvaughn1230@gmail.com>
2023-08-04 12:29:21 -0700

    added validation for user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,11 +9,16 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.signup(email, password);
+
     const token = createToken(user._id);
+
     res.cookie("jwt", token, { httpOnly: true });
+
     res.status(201).json({ user, token });
   } catch (error) {
+    console.log(error);
+
     res.status(400).json(error);
   }
 };
@@ -25,7 +30,9 @@ const loginUser = async (req, res) => {
     const user = await User.login(email, password);
 
     const token = createToken(user._id);
+
     res.cookie("jwt", token, { httpOnly: true });
+
     res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json(error);
